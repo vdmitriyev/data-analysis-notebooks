4b29b13a57c95a50f6dfd4b926d2a32d77a10803
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2018-04-30 18:17:42 +0200

    ADD: A script that compress the files into a zip archive.

---
+++ hana/compress.cmd
@@ -0,0 +1,2 @@
+SET PATH=c:\Soft\7-Zip\;%PATH%
+7z a -tzip archives\sap-hana-and-python-analysis-basics.zip sap-hana-and-python-analysis-basics.html sap-hana-and-python-analysis-basics.ipynb requirements.txt -r data
